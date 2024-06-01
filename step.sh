dea5e2477690d47758f9b4bea7efe084cd7241ef
asa08 <mn.6182.nm@gmail.com>
2024-06-01 17:46:50 +0900

    文言を修正

--- step.sh
+++ step.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-# fail if any commands fails
-set -e
-# debug log
-set -x
-
 if hash jq 2>/dev/null; then
   echo "jq already installed."
 else
@@ -12,11 +6,11 @@ else
   brew install jq
 fi
 
-# 現在のビルドのPR番号を取得します
+# Get PR Number
 CURRENT_PR_NUMBER=$(curl -H "Authorization: ${bitrise_access_token}" https://api.bitrise.io/v0.1/apps/$bitrise_app_slug/builds/$bitrise_build_slug | jq -r '.data.pull_request_id')
-# 同じアプリのすべての実行中のビルドを取得します
+# Get Running Builds
 RUNNING_BUILDS=$(curl -H "Authorization: ${bitrise_access_token}" https://api.bitrise.io/v0.1/apps/$bitrise_app_slug/builds?status=0&workflow=$workflow)
-# 現在のビルドと同じPRを自分以外すべてのビルドをキャンセルします
+# Stop Same Builds
 for BUILD_SLUG in $(echo $RUNNING_BUILDS | jq -r '.data[] | select(.slug != "'$bitrise_build_slug'" and .pull_request_id == '$CURRENT_PR_NUMBER') | .slug'); do
     curl -X 'POST' \
       https://api.bitrise.io/v0.1/apps/$bitrise_app_slug/builds/$BUILD_SLUG/abort \
@@ -24,7 +18,7 @@ for BUILD_SLUG in $(echo $RUNNING_BUILDS | jq -r '.data[] | select(.slug != "'$b
       -H "accept: application/json" \
       -H "Content-Type: application/json" \
       -d '{
-           "abort_reason": "新しいPRを出したため",
+           "abort_reason": "New workflows have been started",
            "abort_with_success": true,
            "skip_notifications": true
           }'
